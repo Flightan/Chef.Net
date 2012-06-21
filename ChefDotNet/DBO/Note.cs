facb3329e41e5e4ef0616513987307e77f5a5610
softcadbury <romain.parage@gmail.com>
2012-06-21 18:03:50 +0200

    ajout de l'objet dbo.note et de la page recherche avancée

---
+++ ChefDotNet/DBO/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DBO
+{
+    public class Note
+    {
+        public int NoteId { get; set; }
+
+        public int NoteRecette { get; set; }
+
+        public int idRecette { get; set; }
+
+        public int idUser { get; set; }
+    }
+}
